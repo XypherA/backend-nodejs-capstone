427a8e2198799eb72061cfa7d037f3fc55a31246
XypherA <aiemanshazwannPG@gmail.com>
2025-11-04 18:32:20 +0800

    Implemented search endpoint and tested all routes

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
-
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
@@ -40,7 +39,7 @@ app.use(pinoHttp({ logger }));
 // Use Routes
 // app.use('/api/auth', authRoutes); // Step 2: add authRoutes to server
 app.use('/api/secondchance/items', secondChanceItemsRoutes); // Task 2: add secondChanceItemsRoutes to server
-app.use('/api/search', searchRoutes); // Task 2: add searchRoutes to server
+app.use('/api/secondchance/search', searchRoutes); // Task 2: add searchRoutes to server
 
 // Global Error Handler
 app.use((err, req, res, next) => {
